f1ce2c081e23f1c49fcd93fcb47c50394234c17b
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-01-27 19:26:19 +0100

    Added a script for pulling docker images

---
+++ pull.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ">> Pulling HDFS images"
+docker compose -f hdfs/docker-compose.yml pull
+
+echo ">> Pulling Spark images"
+docker compose -f spark/docker-compose.yml pull
+
+echo ">> Pulling MongoDB images"
+docker compose -f mongodb/docker-compose.yml pull
+
+echo ">> Pulling Airflow images"
+docker compose -f airflow/docker-compose.yml pull
+
+echo Cluster is up!
