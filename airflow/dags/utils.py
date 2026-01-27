9777c58ee3dbd2315787d66c2da8bb0f16ae83b7
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-01-27 17:46:10 +0100

    Updated utils.py

--- airflow/dags/utils.py
+++ airflow/dags/utils.py
@@ -1,8 +1,12 @@
 import os
+from airflow.sdk import Variable, Connection
 
-def spark_submit(job_path: str, master: str = os.getenv("AIRFLOW_CONN_SPARK_CONNECTION"), args: list = []):
+def spark_submit(job_path: str, master: Connection = Connection.get("SPARK_CONNECTION"), args: list = []):
     airflow_home = os.getenv("AIRFLOW_HOME", "/opt/airflow")
-    command = f"spark-submit --master {master} {airflow_home}/files/spark/{job_path}"
+    command = f"spark-submit --master {master.get_uri()} {airflow_home}/files/spark/{job_path}"
     for arg in args:
         command += f" {arg}"
     return command
+
+def get_hdfs_host():
+    return Connection.get("HDFS_CONNECTION").get_uri()
