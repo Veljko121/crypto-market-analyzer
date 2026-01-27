a03450df73129db4461cfef5d3ad86609590e7e0
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-01-27 18:53:12 +0100

    Added get_mongo_host and updated spark_submit

--- airflow/dags/utils.py
+++ airflow/dags/utils.py
@@ -1,12 +1,18 @@
 import os
-from airflow.sdk import Variable, Connection
+from airflow.sdk import Connection
 
-def spark_submit(job_path: str, master: Connection = Connection.get("SPARK_CONNECTION"), args: list = []):
+def spark_submit(job_path: str, master: Connection = Connection.get("SPARK_CONNECTION"), packages: list = [], args: list = []):
     airflow_home = os.getenv("AIRFLOW_HOME", "/opt/airflow")
-    command = f"spark-submit --master {master.get_uri()} {airflow_home}/files/spark/{job_path}"
+    command = f"spark-submit --master {master.get_uri()}"
+    if len(packages) > 0:
+        command += " --packages " + ",".join(packages)
+    command += f" {airflow_home}/files/spark/{job_path}"
     for arg in args:
         command += f" {arg}"
     return command
 
 def get_hdfs_host():
     return Connection.get("HDFS_CONNECTION").get_uri()
+
+def get_mongo_host():
+    return os.getenv("AIRFLOW_CONN_MONGO_CONNECTION")
