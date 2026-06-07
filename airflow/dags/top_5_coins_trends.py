be9e21249177e73ed9133d699ef492406d098b28
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-06-07 17:32:01 +0200

    Top 5 coins trends

---
+++ airflow/dags/top_5_coins_trends.py
@@ -0,0 +1,39 @@
+from airflow.sdk import dag, task, Param
+from utils import spark_submit, HDFS_HOST, MONGO_HOST, transform_raw_csv_data
+
+@dag(
+    dag_id="top_5_coins_trends",
+    schedule=None,
+    catchup=False,
+    params={
+        "transform_data": Param(False, type="boolean"),
+        "start_date": Param("2013-04-28", type="string", format="date"),
+        "end_date": Param("2021-07-31", type="string", format="date"),
+        "number_of_coins": Param(5, type="number"),
+    }
+)
+def top_5_coins_trends():
+
+    @task.bash
+    def transform_data(**context) -> str:
+        transform_data_boolean = bool(context["params"]["transform_data"])
+        if not transform_data_boolean:
+            return 'echo "Skipping transform"'
+        return transform_raw_csv_data()
+    
+    @task.bash
+    def calculate_top_5_trends(**context):
+        params = context["params"]
+        start_date = params["start_date"]
+        end_date = params["end_date"]
+        number_of_coins = params["number_of_coins"]
+        historical_data = HDFS_HOST + "/transformed_data/csv/historical.csv"
+        coins_data = HDFS_HOST + "/transformed_data/csv/coins.csv"
+        mongo_host = MONGO_HOST
+        mongo_db = "crypto"
+        command = spark_submit("my_jobs/top_5_coins_trends.py", packages=["org.mongodb.spark:mongo-spark-connector_2.13:10.6.0"], args=[historical_data, coins_data, start_date, end_date, number_of_coins, mongo_host, mongo_db])
+        return command
+    
+    transform_data() >> calculate_top_5_trends()
+
+top_5_coins_trends()
---
+++ airflow/files/spark/my_jobs/top_5_coins_trends.py
@@ -0,0 +1,91 @@
+from pyspark.sql import SparkSession
+import sys
+from pyspark.sql.functions import col, lower, sum as spark_sum, count, round as spark_round, row_number, greatest, lit
+from pyspark.sql.window import Window
+from datetime import datetime
+
+def main():
+    historical_data_path = sys.argv[1]
+    coins_data_path = sys.argv[2]
+    start_date = sys.argv[3]
+    end_date = sys.argv[4]
+    number_of_coins = int(sys.argv[5])
+    mongo_host = sys.argv[6]
+    mongo_db = sys.argv[7]
+
+    max_data_points = 2000
+    points_per_coin = max_data_points / number_of_coins
+    total_days = (datetime.strptime(end_date, "%Y-%m-%d") - datetime.strptime(start_date, "%Y-%m-%d")).days
+
+    spark = SparkSession.builder.appName("top_5_coins_trends").config("spark.mongodb.write.connection.uri", mongo_host).getOrCreate()
+
+    historical_df = spark.read.csv(historical_data_path, inferSchema=True, header=True)
+    coins_df = spark.read.csv(coins_data_path, inferSchema=True, header=True)
+
+    df = (
+        historical_df
+        .join(
+            coins_df, on=historical_df.coin_id == coins_df.id, how="inner"
+        )
+        .filter(
+            (col("date") >= start_date) & (col("date") <= end_date) &
+            (~lower(col("name")).like("%innovative%"))
+        )
+        .withColumn("mc", col("price") * col("circulating_supply"))
+    )
+
+    df_top_coins = (
+        df
+        .groupBy(
+            "coin_id", "name"
+        )
+        .agg(
+            spark_sum("mc").alias("mc_summed")
+        )
+        .orderBy(
+            col("mc_summed").desc()
+        )
+        .limit(number_of_coins)
+    )
+
+    result = (
+        df
+        .join(
+            df_top_coins.select("coin_id"), on="coin_id"
+        )
+        .select(
+            "name", "date", "mc"
+        )
+        .orderBy(
+            "date"
+        )
+    )
+
+    # Downsampling
+    coin_days_df = result.groupBy("name").agg(count("date").alias("coin_days"))
+    coin_step_df = coin_days_df.withColumn(
+        "step",
+        greatest(
+            spark_round(
+                col("coin_days") / spark_round(points_per_coin * col("coin_days") / total_days, 0)
+            ).cast("int"),
+            lit(1)
+        )
+    )
+    row_window = Window.partitionBy("name").orderBy("date")
+    result_downsampled = (
+        result
+        .withColumn("rn", row_number().over(row_window))
+        .join(coin_step_df.select("name", "step"), on="name")
+        .filter((col("rn") - 1) % col("step") == 0)
+        .drop("rn", "step")
+    )
+
+    # Writing results
+    result_downsampled.write.format("mongodb").mode("overwrite").option("database", mongo_db).option("collection", "top_5_trends").save()
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
