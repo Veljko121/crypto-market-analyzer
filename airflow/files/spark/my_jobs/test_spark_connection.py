f57b1b9299034df17d145db9bc4f6a867cd01f39
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-06-14 22:03:01 +0200

    Removed unused dag

--- airflow/files/spark/my_jobs/test_spark_connection.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-def main():
-    spark = SparkSession.builder.appName("spark_connection_test").getOrCreate()
-    spark.stop()
-
-if __name__ == "__main__":
-    main()
